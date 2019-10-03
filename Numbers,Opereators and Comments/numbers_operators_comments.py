d8199837f699d82ecf63e65c5514767d8da5a65f
Brian Sekwakwa <briansenpai14@gmail.com>
2019-10-03 15:21:25 +0200

    completed the numbers and operators section

---
+++ Numbers,Opereators and Comments/numbers_operators_comments.py
@@ -0,0 +1,16 @@
+# Floats
+# Basically floats are decimal numbers
+# 1.33 0.5 4.5 8.9
+
+# Integers
+# All whole numbers positive and negative 
+#  1 5 9 -5 -100
+
+# Math Operations
+# + (addition)
+# - (subtraction)
+# / (division - it turns the answer into a float)
+# * (Multiplication)
+# ** (Exponetiation)
+# % (Modulo - gets the remainder of)
+# // (Interger Divsion)
