9e5ef4eb3b595f1ff64b444f8b4749d950b5532d
Brian Sekwakwa <briansenpai14@gmail.com>
2019-10-04 07:07:47 +0200

    finished variables

---
+++ Variables and Strings/variables.py
@@ -0,0 +1,25 @@
+# Variables are just containers that store 1 data type at any time which you can recall it later when you need to later in the code
+
+# Example
+
+x = 100
+y = 500
+
+coke, fanta, sprite = 200, 400, 600
+
+z = x + y
+
+print(z)
+print(coke, fanta, sprite)
+
+# Variable Naming Restrictions
+
+# 1. Variables must start with a letter or underscore
+# 2. The rest of the variable name must only have letters, numbers and underscores (No special characters)
+# 3. Names are case-sensitive
+
+# Naming Conventions
+
+# 1. Variables should snake_case (underscores between words)
+# 2. Variables should be lowercase with some exceptions
+# 3. Variables that start and end with two underscores called "Double UNDERscore" DUNDER, supposed to be private or left alone __no_touchy__
