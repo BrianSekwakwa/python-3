cf8aae7ce85dca3f6d81a4b3f2c893b923d99da6
Brian Sekwakwa <briansenpai14@gmail.com>
2019-10-07 16:01:48 +0200

    completed the string data type

---
+++ Variables and Strings/data_types.py
@@ -0,0 +1,7 @@
+# Data Types are the foundation of all programming languages
+
+# bool - true or false
+# int - integer (1,2,3)
+# str - string, sequence of unicode characters
+# list - an ordered sequence of other data types 
+# dict - collection of key:value pairs
