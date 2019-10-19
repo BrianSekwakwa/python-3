37370660e7f5c4a413327cd096ceb6efcff7112a
Brian Sekwakwa <briansenpai14@gmail.com>
2019-10-19 10:01:31 +0200

    completed the list section

--- Lists Comprehension/list_comprehension.py
+++ Lists Comprehension/list_comprehension.py
@@ -14,10 +14,18 @@
 
 # / Lists Comprehension with Conditional Logic
 
-numbers = [1,2,3,4,5,6]
+# numbers = [1,2,3,4,5,6]
 
-evens = [num for num in numbers if num % 2 == 0]
+# evens = [num for num in numbers if num % 2 == 0]
 
-odds = [num for num in numbers if num % 2 != 0]
+# odds = [num for num in numbers if num % 2 != 0]
 
-print(evens,odds) 
+# print(evens,odds) 
+
+# / Nested Lists, basically lists within lists
+
+nested_list = [[1,2,3],[4,5,6],[7,8,9]]
+
+for item in nested_list:
+  for num in item:
+    print(f"{num} comes from this list: {item}")
